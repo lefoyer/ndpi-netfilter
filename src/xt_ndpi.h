04bb6307d59fe90bfcf2d8353180a17f09d13f46
qiuzi <gxfclql@gmail.com>
2013-08-26 04:12:25 +0800

    update 6707

--- src/xt_ndpi.h
+++ src/xt_ndpi.h
@@ -35,7 +35,7 @@ struct xt_ndpi_mtinfo {
 "TDS","DirectDownloadLink","I23V5","AppleJuice","DirectConnect","Socrates","WinMX","VMware","PANDO","Filetopia",\
 "iMESH","Kontiki","OpenFT","Kazaa","Gnutella","eDonkey","Bittorrent","OFF","AVI","Flash",\
 "OGG","MPEG","QuickTime","RealMedia","Windowsmedia","MMS","XBOX","QQ","MOVE","RTSP",\
-"Feidian","Icecast","PPLive","PPStream","Zattoo","SHOUTCast","SopCast","TVAnts","TVUplayer","VeohTV",\
+"IMAPS","Icecast","PPLive","PPStream","Zattoo","SHOUTCast","SopCast","TVAnts","TVUplayer","VeohTV",\
 "QQLive","Thunder","Soulseek","GaduGadu","IRC","Popo","Unencrypted_Jabber","MSN","Oscar","Yahoo",\
 "Battlefield","Quake","VRRP","Steam","Halflife2","World_of_Warcraft","Telnet","STUN","IPSEC","GRE",\
 "ICMP","IGMP","EGP","SCTP","OSPF","IP_in_IP","RTP","RDP","VNC","PCAnywhere",\
@@ -45,7 +45,7 @@ struct xt_ndpi_mtinfo {
 "DropBox","Gmail","Google_Maps","YouTube","Skype","Google","DCE_RPC","NetFlow_IPFIX","sFlow","HTTP_Connect_SSL_over_HTTP",\
 "HTTP_Proxy","Citrix","Netflix","LastFM","Grooveshark","SkyFile_Prepaid","SkyFile_Rudics","SkyFile_Postpaid","CitrixOnline","Apple_iMessage_FaceTime",\
 "Webex","WhatsApp","Apple_iCloud","Viber","Apple_iTunes","Radius","Windows_Update","TeamViewer","Tuenti","Lotus_Notes",\
-"SAP","GTP","uPnP","LLMNR","Remote_Scan","Spotify","WebM","H323","OpenVPN","NOE","CiscoVPN","TeamSpeak","Tor","Skinny","RTCP","RSYNC","Oracle","Corba","Ubuntu_ONE"
+"SAP","GTP","uPnP","LLMNR","Remote_Scan","Spotify","WebM","H323","OpenVPN","NOE","CiscoVPN","TeamSpeak","Tor","Skinny","RTCP","RSYNC","Oracle","Corba","Ubuntu_ONE","WHOIS_DAS"
 #endif
 
 #ifndef NDPI_PROTOCOL_SHORT_STRING
@@ -54,7 +54,7 @@ struct xt_ndpi_mtinfo {
 "tds","ddl","i23v5","applejuice","directconnect","socrates","winmx","vmware","pando","filetopia",\
 "iMESH","kontiki","openft","fasttrack","gnutella","edonkey","bittorrent","off","avi","flash",\
 "ogg","mpeg","quicktime","realmedia","windowsmedia","mms","xbox","qq","move","rtsp",\
-"feidian","icecast","pplive","ppstream","zattoo","shoutcast","sopcast","tvants","tvuplayer","veohtv",\
+"imaps","icecast","pplive","ppstream","zattoo","shoutcast","sopcast","tvants","tvuplayer","veohtv",\
 "qqlive","thunder","soulseek","gadugadu","irc","popo","jabber","msn","oscar","yahoo",\
 "battlefield","quake","vrrp","steam","hl2","worldofwarcraft","telnet","stun","ipsec","gre",\
 "icmp","igmp","egp","sctp","ospf","ipip","rtp","rdp","vnc","pcanywhere",\
@@ -64,7 +64,7 @@ struct xt_ndpi_mtinfo {
 "dropbox","gmail","gmaps","youtube","skype","google","dcerpc","netflow","sflow","httpconnect",\
 "httpproxy","citrix","netflix","lastfm","grooveshark","skyfileprepaid","skyfilerudics","skyfilepostpaid","citrixonline","apple",\
 "webex","wgatsapp","appleicloud","viber","appleitunes","radius","windowsupdate","teamviewer","tuenti","lotusnotes",\
-"sap","gtp","upnp","llmnr","remotescan","spotify","webm","h323","openvpn","noe","ciscovpn","teamspeak","tor","skinny","rtcp","rsync","oracle","corba","ubuntuone"
+"sap","gtp","upnp","llmnr","remotescan","spotify","webm","h323","openvpn","noe","ciscovpn","teamspeak","tor","skinny","rtcp","rsync","oracle","corba","ubuntuone","whoisdas"
 #endif
 
 #endif /* _LINUX_NETFILTER_XT_NDPI_H */
